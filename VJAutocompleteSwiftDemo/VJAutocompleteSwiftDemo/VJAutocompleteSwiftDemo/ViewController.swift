3bae571c4d2c35915ea68a5d2975f5002e5196d4
Jakub Vlasak <vlasakjakub@gmail.com>
2014-09-14 12:05:37 +0200

    Init demo project

---
+++ VJAutocompleteSwiftDemo/VJAutocompleteSwiftDemo/VJAutocompleteSwiftDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VJAutocompleteSwiftDemo
+//
+//  Created by Jakub Vlasák on 14/09/14.
+//  Copyright (c) 2014 Jakub Vlasak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
